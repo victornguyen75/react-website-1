4223c6ef1ba4757794ad43234d666767a209aa24
Victor Nguyen <vdn140030@gmail.com>
2020-11-07 23:28:15 -0600

    chore(HeroSection.js): remove whitespace

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -10,19 +10,19 @@ function HeroSection() {
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
-        <Button 
-          className='btns' 
-          buttonStyle='btn--outline' 
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
           GET STARTED
         </Button>
-        <Button 
-          className='btns' 
-          buttonStyle='btn--primary' 
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
           buttonSize='btn--large'
         >
-          WATCH TRAILER <i className="far fa-play-circle" />
+          WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
       </div>
     </div>
