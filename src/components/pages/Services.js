648a11856a1d1f771a6c92f3af5ad043972b353c
Victor Nguyen <vdn140030@gmail.com>
2020-11-11 17:37:36 -0600

    feat(Products.js, Services.js, SignUp.js): add Footer component

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import Footer from '../Footer';
 import '../../App.css';
 
 export default function Services() {
-  return <h1 className='services'>SERVICES</h1>;
+  return (
+    <>
+      <h1 h1 className='services'>SERVICES</h1>
+      <Footer />
+    </>  
+  );
 }
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import '../../App.css';
+import Footer from '../Footer';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>;
+  return (
+    <>
+      <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>
+      <Footer />
+    </>
+  );
 }
