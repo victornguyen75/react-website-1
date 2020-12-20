67511b93770ef29a4d95410448c5ebfcf572c35d
Nguyen, Victor <vdn140030@gmail.com>
2020-12-19 19:56:03 -0600

    fix(App.css, SignUp.js): fix the background image and text

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -5,7 +5,7 @@ import Footer from '../Footer';
 export default function SignUp() {
   return (
     <>
-      <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>
+      <h1 className='sign-up'>SIGN UP</h1>
       <Footer />
     </>
   );
