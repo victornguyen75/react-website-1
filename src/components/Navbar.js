400504d1b14a6843047612bd8cd8bf125001eb45
Nguyen, Victor <vdn140030@gmail.com>
2020-12-19 20:11:44 -0600

    fix(Navbar.js): fix routing issues to home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,7 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+          <Link to='/home' className='navbar-logo' onClick={closeMobileMenu}>
             TRVL
             <i className='fab fa-typo3' />
           </Link>
@@ -33,7 +33,7 @@ function Navbar() {
           </div>
           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
             <li className='nav-item'>
-              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              <Link to='/home' className='nav-links' onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
