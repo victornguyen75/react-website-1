d10ef3c8fac50dfaa3487abcbef056bd2ff5ab28
Nguyen, Victor <vdn140030@gmail.com>
2020-12-19 19:58:07 -0600

    Updates

---
+++ precache-manifest.49de21832958b59fd87e364b753ee653.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6a2c0d02272a7bc3af921b578da9a8",
+    "url": "/react-website-1/index.html"
+  },
+  {
+    "revision": "6c77e9f37b9f227c1ddd",
+    "url": "/react-website-1/static/css/main.bd6560ac.chunk.css"
+  },
+  {
+    "revision": "e6bb9b06530630018718",
+    "url": "/react-website-1/static/js/2.40a5925b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-website-1/static/js/2.40a5925b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c77e9f37b9f227c1ddd",
+    "url": "/react-website-1/static/js/main.de7bc689.chunk.js"
+  },
+  {
+    "revision": "ad0743cdb07e89dbaa84",
+    "url": "/react-website-1/static/js/runtime-main.ab7ffcdf.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/react-website-1/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/react-website-1/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/react-website-1/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/react-website-1/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/react-website-1/static/media/img-8.d211c298.jpg"
+  },
+  {
+    "revision": "523a89f64e53deb7aa02e9ff0fa6d18f",
+    "url": "/react-website-1/static/media/img-9.523a89f6.jpg"
+  },
+  {
+    "revision": "229f5279475d67e3cf902f7ab5cd818b",
+    "url": "/react-website-1/static/media/video-1.229f5279.mp4"
+  }
+]);
